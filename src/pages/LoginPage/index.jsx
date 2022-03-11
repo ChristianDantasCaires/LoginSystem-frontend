780e8d0a7dbd57af40a6d9f7f669c01baec54cb3
Chr04 <christiandc99@gmail.com>
2022-03-11 16:53:40 -0300

    iniciando tela de login

--- src/pages/LoginPage/index.jsx
+++ src/pages/LoginPage/index.jsx
@@ -1,10 +1,25 @@
+import "./styles.css";
 
 const LoginPage = () => {
 
     return (
-        <form>
-            login
-        </form>
+        <div id="login">
+            <div className="title">Login</div>
+            <form className="form">
+                <div className="field">
+                    <label htmlFor="email">Email</label>
+                    <input type="email" name="email" id="email" />
+                </div>
+                <div className="field">
+                    <label htmlFor="password">Password</label>
+                    <input type="password" name="password" id="password" />
+                </div>
+
+                <div className="actions">
+                    <button>Entrar</button>
+                </div>
+            </form>
+        </div>
     );
 }
 
